ec1999f009950e280afd2e75ea40c4f72fe62f1f
Riccardo Coppola <riccardo.coppola@burberry.com>
2019-05-01 15:31:01 +0100

    Fixes typo in tests

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,7 +1,7 @@
 const assert = require('assert');
 const BASE_URL = process.env.BASE_URL;
 
-describe('webdriver.io page', () => {
+describe('Test page', () => {
   it('should have the right title', () => {
     browser.url(BASE_URL);
     const title = browser.getTitle();
